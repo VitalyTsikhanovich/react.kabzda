28ecabaebdde5358337aef7e6c8d1b1dba48f0d9
tihon <tihonnet@gmail.com>
2021-11-20 11:44:00 +0300

    feat: UseState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+export default {
+    title: "component/useState demo"
+}
+
+function generateData() {
+    console.log("generateData")
+    return 1
+}
+
+export const Example1 = () => {
+    console.log("Example1")
+
+    //const initValue = useMemo(generateData, [])
+
+    const [counter, setCounter] = useState(generateData)
+
+    return <>
+        <button onClick={() => setCounter((state) => state + 1)}>+</button>
+        {counter}
+    </>
+}
